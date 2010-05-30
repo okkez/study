383551deed68c5d637ae436520253c01f19c7ff4
okkez <okkez000@gmail.com>
2010-05-30 15:45:29 +0900

    refactor

--- ruby/refactor_book/chap01/movie.rb
+++ ruby/refactor_book/chap01/movie.rb
@@ -24,6 +24,6 @@ class Movie
   end
 
   def frequent_renter_points(days_rented)
-    (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
+    @price.frequent_renter_points(days_rented)
   end
 end
--- ruby/refactor_book/chap01/price.rb
+++ ruby/refactor_book/chap01/price.rb
@@ -1,5 +1,13 @@
 
+
+module DefaultPrice
+  def frequent_renter_points(days_rented)
+    1
+  end
+end
+
 class RegularPrice
+  include DefaultPrice
   def charge(days_rented)
     result = 2
     result += (days_rented - 2) * 1.5 if days_rented > 2
@@ -11,9 +19,14 @@ class NewReleasePrice
   def charge(days_rented)
     days_rented * 3
   end
+
+  def frequent_renter_points(days_rented)
+    days_rented > 1 ? 2 : 1
+  end
 end
 
 class ChildrensPrice
+  include DefaultPrice
   def charge(days_rented)
     result = 1.5
     result += (days_rented - 3) * 1.5 if days_rented > 3
